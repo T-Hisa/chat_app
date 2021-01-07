2997a32e9fde9c92998166f3de20bbded651760a
Takaaki <dacchin.h081229@gmail.com>
2021-01-07 19:03:17 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueI18n from 'vue-i18n'
+
+Vue.use(Vuex)
+Vue.use(BootstrapVue)
+Vue.use(VueI18n)
+
+const messages = {
+  ja: {
+
+  }
+}
+let i18n = new VueI18n({
+  locale: 'ja',
+  messages: messages,
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  i18n: i18n,
+  router,
+  components: { App },
+  template: '<App/>'
+})
